5d1bbcbed18763b5ffb8cede73b28b8206294a5c
La Mamaril <lamamaril11@gmail.com>
2025-06-08 22:06:25 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Navigation from "./pages/Navigation"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Grizzly from "./pages/Grizzly";
+function App() {
+ 
+
+  return (
+    
+     <>
+      <Navigation/>
+      <Grizzly/>
+     </>
+  
+  )
+}
+
+export default App
